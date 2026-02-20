16548ad4c49e6fc68cf1e69c3ec2d14da4b7d929
Serge Svalov <wndrsw@gmail.com>
2026-02-20 17:06:10 +0200

    fix

--- game_logic.py
+++ game_logic.py
@@ -9,17 +9,25 @@ def init_game(session):
     session['current_lang'] = 'ru'
 
 def update_game_state(session, scene_data):
+    state_changed = False
+    
     if 'loot' in scene_data:
         for item in scene_data['loot']:
             if item not in session['inventory']:
                 session['inventory'].append(item)
+                state_changed = True
     
     if 'consume_loot' in scene_data:
         for item in scene_data['consume_loot']:
             if item in session['inventory']:
                 session['inventory'].remove(item)
+                state_changed = True
     
     if 'new_companion' in scene_data:
         companion = scene_data['new_companion']
         if companion not in session['companions']:
             session['companions'].append(companion)
+            state_changed = True
+            
+    if state_changed:
+        session.modified = True
